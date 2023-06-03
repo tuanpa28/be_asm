7d8f037cbe050ecc0991ffe663073e9ff6ed3f41
tuanpa03 <phamanhtuan2833@gmail.com>
2023-06-03 11:58:31 +0700

    feact: upload, delete image refs: AW-23

---
+++ src/routers/upload.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import multer from "multer";
+import { uploadImage, updateImage, deleteImage } from "../controllers/upload.js";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
+
+const router = Router();
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "node_react",
+    // format: "png",
+  },
+});
+
+const upload = multer({ storage });
+
+router.post("/images/upload", upload.array("image", 5), uploadImage);
+router.put("/images/:publicId", upload.array("image", 5), updateImage);
+router.delete("/images/:publicId", deleteImage);
+
+export default router;
