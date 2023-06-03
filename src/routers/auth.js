6a8df6e2d5b0897b8cfc998625f74a50ab8f67b2
tuanpa03 <phamanhtuan2833@gmail.com>
2023-06-03 11:39:28 +0700

    feat: signin, signup api refs: AW-26

---
+++ src/routers/auth.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { signup, signin } from "../controllers/auth.js";
+const router = Router();
+
+router.post("/signup", signup);
+router.post("/signin", signin);
+
+export default router;
