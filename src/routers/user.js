4b0c09046f8c418f776505b90f7f27bea1620f0c
quyetthang22 <thangdqph20775@fpt.edu.vn>
2023-06-14 23:50:23 +0700

    feat: comment refs: AW-52

--- src/routers/user.js
+++ src/routers/user.js
@@ -5,11 +5,13 @@ import {
   updateUserProfile,
   updateCart,
   deleteCart,
+  comment
 } from "../controllers/user.js";
 
 const router = Router();
 
 router.get("/user/profile", authenticate, getUserProfile);
+router.post("/user/comment", authenticate, comment);
 router.put("/user/update", authenticate, updateUserProfile);
 router.put("/cart", authenticate, updateCart);
 router.delete("/cart/:id", authenticate, deleteCart);
